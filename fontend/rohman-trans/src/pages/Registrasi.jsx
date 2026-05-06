0e2df061134f4b698308ab4fccb06fbe258f9d6b
fauzanperwira444-netizen <fauzanperwira444@gmail.com>
2026-05-06 14:02:41 +0700

    Penambahan useState pada login dan register page

--- fontend/rohman-trans/src/pages/Registrasi.jsx
+++ fontend/rohman-trans/src/pages/Registrasi.jsx
@@ -19,6 +19,48 @@ export function Registrasi(){
       [e.target.name]: e.target.value
      });
     }
+     function validate(form) {
+        const errors = {};
+
+        
+        if (!form.nama) {
+            errors.nama = "Nama wajib diisi";
+        } 
+        if (!form.email) {
+            errors.email = "Email wajib diisi";
+        } else if (!form.email.includes("@")) {
+            errors.email = "Format email tidak valid";
+        }
+
+        if (!form.password) {
+            errors.password = "Password wajib diisi";
+        } else {
+        const pwdRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[._!])[A-Za-z0-9._!]{8,20}$/;
+        if (!pwdRegex.test(form.password)) {
+            errors.password = "Password harus 8-20 karakter, mengandung minimal 1 huruf besar, 1 angka, dan 1 simbol (.,_!)";
+        }
+    }
+        if (!form.verfpwd) {
+            errors.verfpwd = "Lakukan konfirmasi password";
+        } 
+
+        return errors;
+    }
+
+     const [errors, setErrors] = useState({});
+
+    function handleSubmit(e) {
+    e.preventDefault();
+
+    const validationErrors = validate(form);
+
+    if (Object.keys(validationErrors).length > 0) {
+        setErrors(validationErrors);
+        return;
+    }
+
+    console.log("Form valid:", form);
+    }
 
     return(
         <>
@@ -31,7 +73,7 @@ export function Registrasi(){
                             Buat akun Rohman Trans milikmu
                         </p>
                     </div>
-                <form >
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-12 col-sm-6 col-lg-12 input-css">
                             <InputField
@@ -39,38 +81,42 @@ export function Registrasi(){
                                 name="nama"
                                 value={form.nama}
                                 onChange={handleChange}
+                                error={errors.nama}
                                 placeholder="Masukan Nama Lengkap"
                             />
                         </div>
 
-                         <div className="col-12 col-sm-6 col-lg-12 ">
+                         <div className="col-12 col-sm-6 col-lg-12 input-css ">
                             <InputField
-                                label="email"
+                                label="Email"
                                 name="email"
                                 value={form.email}
                                 onChange={handleChange}
+                                error={errors.email}
                                 placeholder="nama@gmail.com"
                             />
                         </div>
 
-                        <div className="col-12 col-sm-6 col-lg-12 ">
+                        <div className="col-12 col-sm-6 col-lg-12 input-css">
                             <InputField
                                 label="Password"
                                 name="password"
                                 type="password"
                                 value={form.password}
-                                onChange={handleChange}     
+                                onChange={handleChange} 
+                                error={errors.password}    
                                 placeholder="Masukan password"
                             />
                         </div>
 
-                        <div className="col-12 col-sm-6 col-lg-12 ">
+                        <div className="col-12 col-sm-6 col-lg-12 input-css">
                             <InputField
                                 label="Konfirmasi Password"
-                                name="password"
+                                name="verfpwd"
                                 type="password"
                                 value={form.verfpwd}
-                                onChange={handleChange}       
+                                onChange={handleChange} 
+                                error={errors.verfpwd}      
                                 placeholder="Masukan password"
                             />  
                         </div>
